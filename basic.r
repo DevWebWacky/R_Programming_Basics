5a87529ff0782506b8912971da9077ba8d919742
Uwakmfon Paul <122984385+DevWebWacky@users.noreply.github.com>
2025-11-12 01:25:56 +0000

    Enhance basic.r with data handling comments
    Added comments and explanations for data handling in R.

--- basic.r
+++ basic.r
@@ -13,3 +13,23 @@ print (a)
 ls ()
 [1] "a" "b" "c"
 help ("log") #getting help in R 
+data () #this shows all the default or inbuilt dataset in R
+#Once the dataset has been loading, just type the name of the dataset below the "data ()" function and it will be loaded. 
+
+#TYPES OF DATA
+#For instance:
+a <- 1
+class(a)
+#The most common ways of storing data in R is using dataframes (they are like tables)
+library(dslabs)
+data("murders")
+class(murders)
+[1] "data.frame"
+#To find out more about an object or you want to know the structure, you use the "str" function which stands for structure.
+str(murders)
+#To show the first six data in a table you use the "head" function
+head(murders)
+#For our analysis, we will need to access the different variables represented by columns. To access these variables, we use the dollar sign symbol.It's called the accessor.
+murders$population
+#We can also get the names of the columns using the names function.
+names(murders) #tells us the five names of the five columns.
